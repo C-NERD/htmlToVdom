3a07c3ca02239614328f4707cca251272e0fc160
C-NERD <alayaa694@gmail.com>
2021-11-05 12:54:25 +0100

    updated readme and example

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # htmlToVdom
 Karax extension to convert html in string form to embeddable Karax vdom
+
+## Code example
+```Nim
+import ../src/htmlToVdom
+import karax / [karaxdsl, vdom, karax]
+
+let 
+  html = """
+  <nav id = "navbar" style = "background: blue;">Hello This is a navbar</nav>
+  <section id = "mainsection">
+    <pre>
+      <code>
+import htmToVdom, htmlstr
+echo htmlstr.str.toVdom()
+      </code>
+    </pre>
+    <p>Just some incorrect nim code</p>
+  </section>
+  <footer>
+    <div id = "legalsection">
+      <a href = "https://someurl">some text</a>
+      <a href = "https://someurl">some text</a>
+      <a href = "https://someurl">some text</a>
+    <div>
+  </footer>
+  """
+
+proc main() : VNode =
+
+  result = buildHtml(main(id = "ROOT")):
+
+    for dom in toVdom(html):
+      dom
+
+when isMainModule:
+  setRenderer main
+```
--- examples/example1.nim
+++ examples/example1.nim
@@ -1,35 +1,33 @@
 import ../src/htmlToVdom
 import karax / [karaxdsl, vdom, karax]
-#from strutils import strip
 
 let 
-    html = """
-      <nav id = "navbar" style = "background: blue;">Hello This is a navbar</nav>
-      <section id = "mainsection">
-        <pre>
-            <code>
+  html = """
+  <nav id = "navbar" style = "background: blue;">Hello This is a navbar</nav>
+  <section id = "mainsection">
+    <pre>
+      <code>
 import htmToVdom, htmlstr
 echo htmlstr.str.toVdom()
-            </code>
-        </pre>
-        <p>Just some incorrect nim code</p>
-      </section>
-      <footer>
-        <div id = "legalsection">
-            <a href = "https://someurl">some text</a>
-            <a href = "https://someurl">some text</a>
-            <a href = "https://someurl">some text</a>
-        <div>
-      </footer>
-    """
+      </code>
+    </pre>
+    <p>Just some incorrect nim code</p>
+  </section>
+  <footer>
+    <div id = "legalsection">
+      <a href = "https://someurl">some text</a>
+      <a href = "https://someurl">some text</a>
+      <a href = "https://someurl">some text</a>
+    <div>
+  </footer>
+  """
 
 proc main() : VNode =
-    result = buildHtml(main(id = "ROOT")):
 
-        for dom in toVdom(html):
-            dom
-        
-        #text("<strong>Hello boyz</strong>")
+  result = buildHtml(main(id = "ROOT")):
+
+    for dom in toVdom(html):
+      dom
 
 when isMainModule:
-    setRenderer main
+  setRenderer main
